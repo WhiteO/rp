80f58d3febb72968680b19f0c1d0804a8f41a009
whiteo <Splyrgeola25>
2020-01-17 15:22:42 +0300

    Initial commit

---
+++ src/main/java/de/whiteo/RP/RpApplication.java
@@ -0,0 +1,67 @@
+package de.whiteo.RP;
+
+import org.pcap4j.core.*;
+import org.pcap4j.packet.Packet;
+import org.pcap4j.util.NifSelector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class RpApplication {
+
+    //SpringApplication.run(RpApplication.class, args);
+    static PcapNetworkInterface getNetworkDevice() {
+        PcapNetworkInterface device = null;
+        try {
+            device = new NifSelector().selectNetworkInterface();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return device;
+    }
+
+    public static void main(String[] args) throws PcapNativeException, NotOpenException {
+        PcapNetworkInterface device = getNetworkDevice();
+        System.out.println("You chose: " + device);
+
+        // New code below here
+        if (device == null) {
+            System.out.println("No device chosen.");
+            System.exit(1);
+        }
+
+        // Open the device and get a handle
+        int snapshotLength = 65536; // in bytes
+        int readTimeout = 50; // in milliseconds
+        final PcapHandle handle;
+        handle = device.openLive(snapshotLength, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, readTimeout);
+
+
+		// Set a filter to only listen for tcp packets on port 80 (HTTP)
+        String filter = "tcp dst port 1542 and ip[2:2] > 5000";
+        handle.setFilter(filter, BpfProgram.BpfCompileMode.OPTIMIZE);
+
+        // Create a listener that defines what to do with the received packets
+        PacketListener listener = new PacketListener() {
+            @Override
+            public void gotPacket(Packet packet) {
+                // Override the default gotPacket() function and process packet
+                System.out.println(handle.getTimestamp());
+                System.out.println(packet);
+            }
+        };
+
+        // Tell the handle to loop using the listener we created
+        try {
+            int maxPackets = 2000;
+            handle.loop(maxPackets, listener);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Cleanup when complete
+        handle.close();
+    }
+}
