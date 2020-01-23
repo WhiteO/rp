3c89dd8bbdf17a7e2b1c0c737cec00d3558a8c41
m.nemenko <m.nemenko@elama.ru>
2020-01-23 17:26:17 +0300

    test

--- src/main/java/de/whiteo/RP/RpApplication.java
+++ src/main/java/de/whiteo/RP/RpApplication.java
@@ -1,14 +1,28 @@
 package de.whiteo.rp;
 
 import de.whiteo.rp.util.PacketLoop;
+import org.pcap4j.core.*;
+import org.pcap4j.packet.Packet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RpApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PcapNativeException {
         SpringApplication.run(RpApplication.class, args);
-        new PacketLoop().run();
+        final String DEVICE_NAME = "\\Device\\NPF_Loopback";
+        final int MAX_PACKETS = 2000;
+
+        final PcapNetworkInterface device = Pcaps.getDevByName(DEVICE_NAME);
+        final PacketListener listener = System.out::println;
+
+        new PacketLoop(device, listener, handle -> {
+            try {
+                handle.loop(MAX_PACKETS, listener);
+            } catch (PcapNativeException | InterruptedException | NotOpenException e) {
+                e.printStackTrace();
+            }
+        }).run();
     }
 }
--- src/main/java/de/whiteo/RP/util/PacketLoop.java
+++ src/main/java/de/whiteo/RP/util/PacketLoop.java
@@ -1,8 +1,9 @@
 package de.whiteo.rp.util;
 
 import org.pcap4j.core.*;
-import org.pcap4j.packet.Packet;
 import org.slf4j.LoggerFactory;
+
+import java.util.function.Consumer;
 
 /**
  * @author Ruslan Tanas {@literal <skyuser13@gmail.com>}
@@ -10,45 +11,38 @@ import org.slf4j.LoggerFactory;
 
 public class PacketLoop implements Runnable {
 
-    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(PacketLoop.class);
-    int snapshotLength = 65536;
-    int readTimeout = 10;
-    String filter = "tcp dst port 1542 and ip[2:2] > 5000";
+	final PcapNetworkInterface networkInterface;
+	final PacketListener listener;
+    final Consumer<PcapHandle> consumer;
 
-    @Override
-    public void run() {
-        PcapNetworkInterface loopbackNetworkDevice = getLoopbackNetworkDevice();
-        if (loopbackNetworkDevice == null) {
-            logger.error("No device chosen.");
-            System.exit(1);
-        }
+    public PacketLoop(PcapNetworkInterface networkInterface,
+                      PacketListener listener,
+                      Consumer<PcapHandle> consumer) {
+		assert (networkInterface != null);
+		assert (listener != null);
+		assert (consumer != null);
+		this.networkInterface = networkInterface;
+		this.listener = listener;
+		this.consumer = consumer;
+	}
 
-        try (final PcapHandle handle = loopbackNetworkDevice.openLive(snapshotLength,
-                PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, readTimeout)) {
-            handle.setFilter(filter, BpfProgram.BpfCompileMode.OPTIMIZE);
 
-            PacketListener listener = new PacketListener() {
-                @Override
-                public void gotPacket(Packet packet) {
-                    System.out.println(packet);
-                }
-            };
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(PacketLoop.class);
+	int snapshotLength = 65536;
+	int readTimeout = 10;
+	String filter = "tcp dst port 1542 and ip[2:2] > 5000";
 
-            int maxPackets = 2000;
-            handle.loop(maxPackets, listener);
+	@Override
+	public void run() {
 
-        } catch (Exception e) {
-            logger.error(e.toString());
-        }
-    }
-
-    static PcapNetworkInterface getLoopbackNetworkDevice() {
-        PcapNetworkInterface device = null;
-        try {
-            device = Pcaps.getDevByName("\\Device\\NPF_Loopback");
-        } catch (Exception e) {
-            logger.error(e.toString());
-        }
-        return device;
-    }
+		try (final PcapHandle handle = networkInterface.openLive(
+				snapshotLength,
+				PcapNetworkInterface.PromiscuousMode.PROMISCUOUS
+				, readTimeout)) {
+			handle.setFilter(filter, BpfProgram.BpfCompileMode.OPTIMIZE);
+			consumer.accept(handle);
+		} catch (Exception e) {
+			logger.error(e.toString());
+		}
+	}
 }
